69d1a602c5db2bdb10a6bc792b2c347f981d85e4
sziddhant <sziddhant@gmail.com>
2018-10-24 00:55:13 +0530

    //

--- LUIS.py
+++ LUIS.py
@@ -11,9 +11,3 @@ def msg_luis(inp):
     return top_intent
 
 
-def get_message(inp):
-    msg = endpoint + inp
-    ldata = requests.get(msg)
-    k = ast.literal_eval(ldata.text)
-    top_intent = (k['topScoringIntent']['intent'])
-    return top_intent
