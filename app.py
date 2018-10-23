e1be670e54371c0e75185d96a07c50c813fcde67
sziddhant <sziddhant@gmail.com>
2018-10-24 00:30:19 +0530

    //

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from datetime import date
 from datetime import time
 from datetime import datetime
 from data import *
+from old import *
 
 
 app = Flask(__name__)
@@ -49,74 +50,6 @@ def send_message(recipient_id, response):
     return "success"
 
 
-def get_message(inp):
-    """
-    print (message['nlp']['entities']['greetings'][0]['confidence'])
-    x1,x2,x3='','',''
-    if message['nlp']['entities']['greetings'][0]['confidence']> 0.98:
-        x1="Hello :)"
-
-    if message['nlp']['entities']['thanks'][0]['confidence']> 0.98:
-        x2="Your Welcome"
-    if message['nlp']['entities']['bye'][0]['confidence']> 0.98:
-        x3="Bye"
-
-    sample= ['1','2','3']
-    ran=random.choice(sample)
-    xn=x1+x2+x3
-    """
-
-    a, e = '', ''
-    for y1 in hello:
-        if inp.lower() == y1:
-            a = "Hello! :) \nTry Rock paper scisors \nAsk the date or time\nSay toss to do a toss"
-            break
-    for y2 in bye:
-        if inp.lower() == y2:
-            a = "Bye"
-            break
-    for y3 in HAU:
-        if inp.lower() == y3:
-            a = "I'm fine, What about you?"
-            break
-    for y4 in time:
-        if inp.lower() == y4:
-            k = str(datetime.time(datetime.now()))
-            a = "The time is " + k
-            break
-    for y5 in date:
-        if inp.lower() == y5:
-            k = str(datetime.date(datetime.now()))
-            a = "The date is " + k
-            break
-    for y6 in RPS:
-        if inp.lower() == y6:
-            bm = random.choice(RPS)
-            a = bm
-            if (((inp.lower() == 'rock') and bm == 'scissor') or (inp.lower() == 'paper' and bm == 'rock') or (
-                    inp.lower() == 'scissor' and bm == 'paper')):
-                a = bm + ',' + "You win"
-            elif inp.lower() == bm:
-                a = bm + ',' + "It's a tie!"
-            else:
-                a = bm + ',' + "I win"
-            break
-
-    for y6 in toss:
-        if inp.lower() == y6:
-            bm = random.choice(TOSS)
-            a = bm
-            break
-        for y in info:
-            if inp.lower() == y:
-                a = "Chat Bot Test is an expertimental chat bot deployed on Facebook Messenger by Siddhant Saoji \nThis bot is currently in devlpoment phase"
-                break
-
-    if a == '':
-        e = "Sorry this is not supported yet"
-    ans = a + e
-    print(ans)
-    return ans
 
 
 if __name__ == '__main__':
--- old.py
+++ old.py
@@ -1,8 +1,8 @@
-def get_message_old1(inp):
+def get_message(inp):
     a, e = '', ''
     for y1 in hello:
         if inp.lower() == y1:
-            a = "Hello! :) \nTry Rock paper scisors \nAsk the date or time\nSay toss to flip a coin"
+            a = "Hello! :) \nTry Rock paper scisors \nAsk the date or time\nSay toss to do a toss"
             break
     for y2 in bye:
         if inp.lower() == y2:
@@ -44,35 +44,6 @@ def get_message_old1(inp):
             if inp.lower() == y:
                 a = "Chat Bot Test is an expertimental chat bot deployed on Facebook Messenger by Siddhant Saoji \nThis bot is currently in devlpoment phase"
                 break
-
-    for d in menu_m:
-        if inp.lower() == d:
-            a = "(Breakfast)Poha Jalebi|| (Lunch)||Plain Rice, Arhar dal, Lauki Chana,Boondi Raita||(Dinner) Jeera Rice, Moong-Masoor Dal,Chhole Bhature"
-            break
-    for d in menu_t:
-        if inp.lower() == d:
-            a = "(Breakfast)Uttpam, Sambhar, Coconut Chutney|| (Lunch)||Plain Rice, Dal Tadka, Aloo Matar,Seasonal Fruit Papaya||(Dinner) Fried Rice, Dal Tadka,Manchurian Gravy"
-            break
-    for d in menu_w:
-        if inp.lower() == d:
-            a = "(Breakfast)Pyaaz and Azwain Paratha, Aloo Sabji|| (Lunch)||Plain Rice, Arhar dal, Lauki Chana,Boondi Raita,Chhachh||(Dinner) Plain Rice, Arhar Dal,Mix Veg"
-            break
-    for d in menu_th:
-        if inp.lower() == d:
-            a = "(Breakfast)Idli / Fried Idli, Sambhar, Coconut Chutney|| (Lunch)||Plain Rice, Yellow Arhar-Moong dal,Kadhi-Pakoda, Black Chana||(Dinner)Plain Rice, Arhar Dal, Red Pumpkin,Gulab Jamun/ Kala Jamun"
-            break
-    for d in menu_f:
-        if inp.lower() == d:
-            a = "(Breakfast)Poori Sabji|| (Lunch)||Lemon Rice, Chana Dal, Bhindi Aloo,Banana,Gauva||(Dinner) Plain Rice, Dal Tadka,Paneer / Egg Curry, Ice Cream Cup#"
-            break
-    for d in menu_s:
-        if inp.lower() == d:
-            a = "(Breakfast) Aloo Paratha, Dahi, Sauce, Pickle|| (Lunch) Fried Rice, Urad Dal,Veg Kolhapuri||(Dinner) Plain Rice, Dal Fry,Baingan Bharta, Boondi Laddu"
-            break
-    for d in menu_sun:
-        if inp.lower() == d:
-            a = "(Breakfast) Masala Dosa, Sambhar, Chutney|| (Lunch)Veg Biryani, Dal Fry, Paalak Aloo,||(Dinner) |Plain Rice, Dal Makhani,Cucumber Raita Paneer / Chicken, Rice Kheer"
-            break
 
     if a == '':
         e = "Sorry this is not supported yet"
