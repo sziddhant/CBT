5d07764774ca362022ceee3a8540c06dfe0b384d
sziddhant <sziddhant@gmail.com>
2018-10-24 00:50:08 +0530

    //

--- imp.py
+++ imp.py
@@ -1,6 +1,6 @@
-key1='1882d386608e4269bf27d7f43ee2c255'
-key2='c147e1a19fa1480aa2aef397b6893360'
-name='BoNLP'
+key1 = '1882d386608e4269bf27d7f43ee2c255'
+key2 = 'c147e1a19fa1480aa2aef397b6893360'
+name = 'BoNLP'
 
-app_id='f4886ba6-e07b-4310-b118-2891564312b6'
-endpoint='https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/f4886ba6-e07b-4310-b118-2891564312b6?subscription-key=887596ea72694b0b8058d477496a22ac&timezoneOffset=-360&q='
+app_id = 'f4886ba6-e07b-4310-b118-2891564312b6'
+endpoint = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/f4886ba6-e07b-4310-b118-2891564312b6?subscription-key=887596ea72694b0b8058d477496a22ac&timezoneOffset=-360&q='
